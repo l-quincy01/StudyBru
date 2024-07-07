87dac091170a36f4a85d0418f6935f1e2ef4a78c
Lesego Pitsi <l.quincypitsi@gmail.com>
2024-07-07 22:34:46 +0200

    Feat: Flash Cards animations added

--- src/Screens/MagicNotes.js
+++ src/Screens/MagicNotes.js
@@ -1,36 +1,31 @@
-import {
-  Alert,
-  Pressable,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import React, { useContext, useState, useEffect } from "react";
+import React, { useState, useRef } from "react";
+import { Animated, Pressable, StyleSheet, Text, View } from "react-native";
 import tw from "twrnc";
 import * as Progress from "react-native-progress";
-import { QuizContext } from "../config/QuizContext";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { flashCards } from "../config/FlashCards";
-import { AntDesign } from "@expo/vector-icons";
 import { FontAwesome5 } from "@expo/vector-icons";
+import { flashCards } from "../config/FlashCards";
 
-const MagicNotes = () => {
+const MagicNotes = ({ navigation }) => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [quizProgress, setQuizProgress] = useState(0);
-
+  const [flashCardSide, setFlashCardSide] = useState(false);
   const progress =
     flashCards.length > 0 ? (currentQuestionIndex + 1) / flashCards.length : 0;
 
-  useEffect(() => {
-    if (flashCards) {
-      setQuizProgress(flashCards.length);
-    }
-  }, [flashCards]);
+  const flipAnim = useRef(new Animated.Value(0)).current;
+  const flipDeg = flipAnim.interpolate({
+    inputRange: [0, 180],
+    outputRange: ["0deg", "180deg"],
+  });
+  const flipDegBack = flipAnim.interpolate({
+    inputRange: [0, 180],
+    outputRange: ["180deg", "360deg"],
+  });
 
   const handleNext = () => {
     if (currentQuestionIndex < flashCards.length - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setFlashCardSide(false);
+      flipAnim.setValue(0); // Reset animation
     } else {
       navigation.navigate("End", { userScore: score });
     }
@@ -39,26 +34,27 @@ const MagicNotes = () => {
   const handlePrev = () => {
     if (currentQuestionIndex > 0) {
       setCurrentQuestionIndex(currentQuestionIndex - 1);
+      setFlashCardSide(false);
+      flipAnim.setValue(0); // Reset animation
     } else {
       navigation.navigate("Splash");
     }
   };
 
-  // if (0 === 0) {
-  //   return (
-  //     <View style={tw`flex-1 items-center justify-center  p-5`}>
-  //       <Text style={tw`text-2xl text-center`}>
-  //         Magic notes will become availble once you generate them
-  //       </Text>
-  //     </View>
-  //   );
-  // }
+  const handleFlashCardSides = () => {
+    Animated.timing(flipAnim, {
+      toValue: flashCardSide ? 0 : 180,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+    setFlashCardSide(!flashCardSide);
+  };
+
   const currentCard = flashCards[currentQuestionIndex];
   return (
-    //Flash Card
-    <View style={tw`flex-1 flex-col items-center justify-center  p-5`}>
+    <View style={tw`flex-1 flex-col items-center justify-center p-5`}>
       <View style={tw`flex-row top-5 absolute`}>
-        <View style={tw`flex-1 `}>
+        <View style={tw`flex-1`}>
           <Progress.Bar
             progress={progress}
             width={null}
@@ -67,14 +63,39 @@ const MagicNotes = () => {
           />
         </View>
       </View>
-      <View
-        style={tw`h-[400px] w-[300px] mt-5 gap-y-3 flex text-center justify-center items-center bg-gray-200  py-2 px-5 rounded-xl`}
-      >
-        <Text style={tw` text-center  font-medium text-lg `}>
-          {currentCard.back}
-        </Text>
-      </View>
-      <View style={tw`items-center justify-between flex flex-row w-full `}>
+      <Pressable onPress={handleFlashCardSides} style={tw`mt-5`}>
+        <View style={styles.flashCardContainer}>
+          <Animated.View
+            style={[
+              styles.flashCard,
+              {
+                transform: [{ rotateY: flipDeg }],
+                zIndex: flashCardSide ? 0 : 1,
+              },
+            ]}
+          >
+            <Text style={tw`text-center font-medium text-lg`}>
+              {currentCard.front}
+            </Text>
+          </Animated.View>
+          <Animated.View
+            style={[
+              styles.flashCard,
+              {
+                transform: [{ rotateY: flipDegBack }],
+                zIndex: flashCardSide ? 1 : 0,
+                position: "absolute",
+                top: 0,
+              },
+            ]}
+          >
+            <Text style={tw`text-center font-medium text-lg`}>
+              {currentCard.back}
+            </Text>
+          </Animated.View>
+        </View>
+      </Pressable>
+      <View style={tw`items-center justify-between flex flex-row w-full`}>
         <Pressable onPress={handlePrev} style={tw`p-5`}>
           <FontAwesome5 name="chevron-left" size={24} color="black" />
         </Pressable>
@@ -85,5 +106,23 @@ const MagicNotes = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  flashCardContainer: {
+    width: 300,
+    height: 400,
+  },
+  flashCard: {
+    height: 400,
+    width: 300,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "gray",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    backfaceVisibility: "hidden",
+  },
+});
 
 export default MagicNotes;
