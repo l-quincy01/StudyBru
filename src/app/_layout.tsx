aa0dde10e1736d2d39686eb2ad2b90e7395596fc
Lesego Pitsi <l.quincypitsi@gmail.com>
2024-12-03 07:16:39 +0200

    Feat: JWT auth added

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -10,7 +10,7 @@ import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
+import { useColorScheme } from "@/src/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
